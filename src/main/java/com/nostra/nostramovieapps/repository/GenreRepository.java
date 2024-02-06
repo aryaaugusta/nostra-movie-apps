1d65ca7f9021b641a0eb8bc9ecc42553055010e9
Arya Augusta <aryaaugusta@gmail.com>
2024-02-06 23:10:10 +0700

    query genre movie get id and name

--- src/main/java/com/nostra/nostramovieapps/repository/GenreRepository.java
+++ src/main/java/com/nostra/nostramovieapps/repository/GenreRepository.java
@@ -1,13 +1,17 @@
 package com.nostra.nostramovieapps.repository;
 
 import com.nostra.nostramovieapps.entity.genre.Genre;
+import com.nostra.nostramovieapps.entity.genre.GenreProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long>, JpaSpecificationExecutor<Genre> {
 
-    /*@Query("SELECT x FROM Genre x WHERE x.name like :name")
-    Optional<Genre> findGenreByNameLike(String name);*/
+    @Query("SELECT g.id as id, g.name as name FROM GENRE g WHERE g.id = :id")
+    GenreProjection getIdAndNameGenre(Long id);
 }
